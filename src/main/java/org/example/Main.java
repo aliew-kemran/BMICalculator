d0679ecf6835153e669c8e47069d7900c9233b87
kemran <52306350@edu.campus02.at>
2025-02-04 21:51:20 +0100

    test final 3.0

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -4,10 +4,10 @@ public class Main {
     public static void main(String[] args) {
 
 
-        BMICalculator bmi = new BMICalculator("Stefan", "Meier", 180, 120, 'm');
-        System.out.println("bmi.calculateBMI() = " + bmi.calculateBMI());
-        System.out.println("bmi.calculateBMICategory() = " + bmi.calculateBMICategory());
-        System.out.println("bmi.getCategoryName() = " + bmi.getBMICategoryName());
+        BMICalculator bmiCalc = new BMICalculator("Stefan", "Meier", 180, 120, 'm');
+        System.out.println("bmiCalc.calculateBMI() = " + bmiCalc.calculateBMI());
+        System.out.println("bmiCalc.calculateBMICategory() = " + bmiCalc.calculateBMICategory());
+        System.out.println("bmiCalc.getCategoryName() = " + bmiCalc.getBMICategoryName());
 
 
 
