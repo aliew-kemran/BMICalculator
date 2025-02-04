2a4c39b60cea92d9b4f8cfd94bd4267cae81c98f
kemran <52306350@edu.campus02.at>
2025-02-04 21:36:59 +0100

    test final

--- src/test/java/org/example/BMICalculatorTest.java
+++ src/test/java/org/example/BMICalculatorTest.java
@@ -7,27 +7,27 @@ class BMICalculatorTest {
     @org.junit.jupiter.api.Test
     void calculateBMI() {
         BMICalculator bmiCalc1 = new BMICalculator("Anna", "Froh", 170, 70.0, 'w');
-        assertEquals(24.22, bmiCalc1.calculateBMI(), 0.01);
+        assertEquals(24.2214, bmiCalc1.calculateBMI());
 
         BMICalculator bmiCalc2 = new BMICalculator("Stefan", "Meier", 180, 120.0, 'm');
-        assertEquals(37.04, bmiCalc2.calculateBMI(), 0.01);
+        assertEquals(37.037037, bmiCalc2.calculateBMI());
     }
 
     @org.junit.jupiter.api.Test
     void calculateBMICategory() {
-        BMICalculator bmiCalc1 = new BMICalculator("Lisa", "Beispiel", 170, 70.0, 'w');
+        BMICalculator bmiCalc1 = new BMICalculator("Anna", "Froh", 170, 70.0, 'w');
         assertEquals(1, bmiCalc1.calculateBMICategory());
 
-        BMICalculator bmiCalc2 = new BMICalculator("Karl", "Groß", 180, 120.0, 'm');
+        BMICalculator bmiCalc2 = new BMICalculator("Stefan", "Meier", 180, 120.0, 'm');
         assertEquals(2, bmiCalc2.calculateBMICategory());
     }
 
     @org.junit.jupiter.api.Test
     void getBMICalculatorName() {
-        BMICalculator bmiCalc1 = new BMICalculator("Paul", "Lang", 170, 70.0, 'w');
+        BMICalculator bmiCalc1 = new BMICalculator("Anna", "Froh", 170, 70.0, 'w');
         assertEquals("Übergewicht", bmiCalc1.getBMICategoryName());
 
-        BMICalculator bmiCalc2 = new BMICalculator("Tim", "Schmidt", 180, 120.0, 'm');
+        BMICalculator bmiCalc2 = new BMICalculator("Stefan", "Meier", 180, 120.0, 'm');
         assertEquals("Sehr starkes Übergewicht", bmiCalc2.getBMICategoryName());
     }
 }
