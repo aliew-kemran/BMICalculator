49e850e98fdebd706144b1e3aa2bda310b8f3dd4
kemran <52306350@edu.campus02.at>
2025-02-04 21:43:57 +0100

    test final 2.0

--- src/test/java/org/example/BMICalculatorTest.java
+++ src/test/java/org/example/BMICalculatorTest.java
@@ -7,10 +7,10 @@ class BMICalculatorTest {
     @org.junit.jupiter.api.Test
     void calculateBMI() {
         BMICalculator bmiCalc1 = new BMICalculator("Anna", "Froh", 170, 70.0, 'w');
-        assertEquals(24.2214, bmiCalc1.calculateBMI());
+        assertEquals(24.2214, bmiCalc1.calculateBMI(), 0.01);
 
         BMICalculator bmiCalc2 = new BMICalculator("Stefan", "Meier", 180, 120.0, 'm');
-        assertEquals(37.037037, bmiCalc2.calculateBMI());
+        assertEquals(37.037037, bmiCalc2.calculateBMI(), 0.01);
     }
 
     @org.junit.jupiter.api.Test
